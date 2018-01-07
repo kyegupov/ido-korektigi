92f56c7fd60d8ba6823c59cb8da341e588ca749f
Konstantin Yegupov <kyegupov4@gmail.com>
2018-01-07 15:38:15 +0000

    Korektigis *regexpo

--- regexala_reguli.js
+++ regexala_reguli.js
@@ -1,6 +1,6 @@
 regexalaReguli = [
     {de:/([0-9]{1,2}) di (januaro|februaro|marto|aprilo|mayo|junio|julio|agosto|septembro|oktobro|novembro|decembro)/, a:"$1 ma di $2"},
-    {de:/[0-9]+a yari/, a:"yari $1ma"},
+    {de:/([0-9]+)a yari/, a:"yari $1ma"},
     {de:/esas ([a-z]+)[ai]ta/, a:"$1esas"},
     {de:/esis ([a-z]+)[ai]ta/, a:"$1esis"},
     {de:/an sk/, a:"ana sk"},
